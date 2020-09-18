065ddffa92d1a53fa6da1e19c240e145153eb2c8
J1aDong <991383877@qq.com>
2020-09-18 09:24:05 +0800

    修改ip

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+from wsgiref.simple_server import make_server
+
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
